ef35e3a9e21f63ebee65be2a4871c65780ba5b1f
Lucas5N <lucastaiano314@gmail.com>
2025-12-28 18:58:22 +0100

    PUSH INIZIALE-4

--- src/main/java/it/unisa/fidelio/dataaccess/ListaRaccomandati.java
+++ src/main/java/it/unisa/fidelio/dataaccess/ListaRaccomandati.java
@@ -3,7 +3,6 @@ package it.unisa.fidelio.dataaccess;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import java.time.Instant;
--- src/test/java/it/unisa/fidelio/FidelioApplicationTests.java
+++ src/test/java/it/unisa/fidelio/FidelioApplicationTests.java
@@ -2,7 +2,6 @@ package it.unisa.fidelio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 /**
  * Forziamo esplicitamente la classe di configurazione.
