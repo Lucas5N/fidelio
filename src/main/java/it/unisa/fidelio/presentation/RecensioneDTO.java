2d24029f574e3c9eaaf95bd40cfa0d9c38ca763c
GuFo0 <forte.guglielmo@gmail.com>
2025-12-30 18:56:36 +0100

    Creazione RecensioneDTO e FilmDTO

---
+++ src/main/java/it/unisa/fidelio/presentation/RecensioneDTO.java
@@ -0,0 +1,14 @@
+package it.unisa.fidelio.presentation;
+
+import java.time.Instant;
+
+public class RecensioneDTO {
+    private Integer id;
+    private String nomeAutore;
+    private String testo;
+    private Double voto;
+    private Instant dataCreazione;
+    private Boolean spoilerAlert;
+    private Integer numLike;
+    private Integer numDislike;
+}
