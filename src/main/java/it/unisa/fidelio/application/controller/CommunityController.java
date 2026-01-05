cd367f3f03116cf53930a55b6c5c491f1f5e0c13
GuFo0 <forte.guglielmo@gmail.com>
2026-01-05 18:11:04 +0100

    UI Community

--- src/main/java/it/unisa/fidelio/application/controller/CommunityController.java
+++ src/main/java/it/unisa/fidelio/application/controller/CommunityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/community")
+@RequestMapping("/communities")
 @CrossOrigin(origins = "*")
 public class CommunityController {
 
